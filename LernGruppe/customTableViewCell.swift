9ca78e4b46798c269a368833dbc73b7ae9ad95ee
BulBul <bulbul@BulBul.fritz.box>
2018-06-04 10:57:11 +0200

    Lerngruppe init

---
+++ LernGruppe/customTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  customTableViewCell.swift
+//  LernGruppe
+//
+//  Created by BulBul on 30.05.18.
+//  Copyright © 2018 BulBul. All rights reserved.
+//
+
+import UIKit
+
+class customTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
